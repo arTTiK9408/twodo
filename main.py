9cd094606a627efcfd4244581f4dbf2ec528f84f
arTTiK9408 <vg9408@gmail.com>
2026-08-16 11:41:08 -0300

    futucando o framework

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header
+# https://textual.textualize.io/widget_gallery/
 
-class TwoDoApp(App):
+# The App class is where most of the logic of Textual apps is written.
+# It is responsible for loading configuration, setting up widgets, handling keys, and more.
+class ToDoApp(App):
     """A TODO.txt manager, with vim motions"""
     BINDINGS = [
         ("j", "navigate_down", "Down"),
         ("k", "navigate_up", "Up"),
         ("q", "quit_app", "Quit"),
-        ("t", "toggle_dark", "Dark mode")
+        ("t", "toggle_dark", "Dark mode"),
+        ("p", "focus_projects", "Projects"),
+        ("c", "focus_context", "Context")
     ]
 
     def compose(self) -> ComposeResult:
         """Create child widget for the app"""
-        yield Header()
-        yield Footer()
+        yield Header(icon = "󰄵")
+        yield Footer(show_command_palette=False)
+
+    def on_mount(self) -> None:
+        self.title = "2do | <placeholder>"
+        self.sub_title = "TODO.txt manager"
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode"""
@@ -22,5 +31,5 @@ class TwoDoApp(App):
         )
 
 if __name__ == "__main__":
-    app = TwoDoApp()
+    app = ToDoApp()
     app.run()
