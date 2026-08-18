7f04ed9730b9dc4b6e99e3f213d3e4a8b9b5d90b
arTTiK9408 <vg9408@gmail.com>
2026-08-17 22:21:00 -0300

    ruff formatting

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header
-# https://textual.textualize.io/widget_gallery/
 
+
+# https://textual.textualize.io/widget_gallery/
 # The App class is where most of the logic of Textual apps is written.
 # It is responsible for loading configuration, setting up widgets, handling keys, and more.
 class ToDoApp(App):
     """A TODO.txt manager, with vim motions"""
-    BINDINGS = [
+
+    BINDINGS = [  # noqa: RUF012
         ("j", "navigate_down", "Down"),
         ("k", "navigate_up", "Up"),
         ("q", "quit_app", "Quit"),
         ("t", "toggle_dark", "Dark mode"),
         ("p", "focus_projects", "Projects"),
-        ("c", "focus_context", "Context")
+        ("c", "focus_context", "Context"),
     ]
 
     def compose(self) -> ComposeResult:
         """Create child widget for the app"""
-        yield Header(icon = "󰄵")
+        yield Header(icon="󰄵")  # TODO: add nerd font requirement check
         yield Footer(show_command_palette=False)
 
     def on_mount(self) -> None:
-        self.title = "2do | <placeholder>"
+        self.title = "TwoDo | <placeholder>"
         self.sub_title = "TODO.txt manager"
 
     def action_toggle_dark(self) -> None:
@@ -29,6 +31,7 @@ class ToDoApp(App):
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
+
 
 if __name__ == "__main__":
     app = ToDoApp()
